7a66d321ceed889179c449adde8f39d0facd9e57
bungas <70490036+Yamuiuurubu@users.noreply.github.com>
2025-11-08 16:28:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const tempo_display = document.getElementById("tempo_placeholder")
+const titulo_display = document.getElementById("titulo")
+
+Notification.requestPermission((result) => {
+  console.log(result);
+});
+
+
+let tempo
+let timer
+
+function tempo_menos_um() {
+  if (tempo > 0) {
+    tempo--
+    //console.log(tempo)
+    tempo_display.textContent = tempo
+    titulo.textContent = tempo
+  } else {
+    clearInterval(timer)
+    const notification = new Notification("ACABOU")
+    //console.log("acabou.")
+  }
+}
+
+function iniciar(tempoInicial = 60) {
+  tempo = tempoInicial
+  tempo_display.textContent = tempo
+  titulo.textContent = tempo
+
+  timer = setInterval(tempo_menos_um, 1000)
+  console.log("Timer iniciado com tempo:", tempo)
+}
+
+function botao() {
+  iniciar(300)
+}
